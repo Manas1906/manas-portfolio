2832688b0b0ea38e6bcbcb24190bdf6d80b5fa2a
Manas9584 <mns.allreach@gmail.com>
2025-12-27 20:13:12 +0530

    some ui changes

--- script.js
+++ script.js
@@ -43,3 +43,25 @@ function eraseEffect() {
 }
 
 document.addEventListener("DOMContentLoaded", typeEffect);
+
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+document.querySelectorAll("#nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("show");
+  });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    document
+      .querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
